1080890a140b7f94c67f4f70e77bf78cb8cf4e46
Davide Segullo <davidesegullo@gmail.com>
2023-12-03 14:17:13 +0100

    feat: :sparkles: add editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,48 +1,46 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'Quirks',
-  tagline: 'Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.',
-  favicon: 'img/favicon.svg',
+  title: "Quirks",
+  tagline:
+    "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+  favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: 'https://quirks-docs.vercel.app',
+  url: "https://quirks-docs.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: "/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'nabla-studio', // Usually your GitHub org/user name.
-  projectName: 'quirks', // Usually your repo name.
+  organizationName: "nabla-studio", // Usually your GitHub org/user name.
+  projectName: "quirks", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/nabla-studio/quirks/tree/main/docs/intro',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
+          editUrl: "https://github.com/nabla-studio/quirks-docs/tree/main/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -50,66 +48,66 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'https://quirks-docs.vercel.app/img/opengraph-image.jpg',
+    image: "https://quirks-docs.vercel.app/img/opengraph-image.jpg",
     navbar: {
       logo: {
-        alt: 'Quirks Logo',
-        src: 'img/quirks-logo-text-light.svg',
-        srcDark: 'img/quirks-logo-text.svg',
+        alt: "Quirks Logo",
+        src: "img/quirks-logo-text-light.svg",
+        srcDark: "img/quirks-logo-text.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Getting Started',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Getting Started",
         },
         {
-          href: 'https://github.com/nabla-studio/quirks',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/nabla-studio/quirks",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Getting Started',
-              to: '/docs',
+              label: "Getting Started",
+              to: "/docs",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Discord',
-              href: 'https://discord.gg/T28vmuTSBD',
+              label: "Discord",
+              href: "https://discord.gg/T28vmuTSBD",
             },
             {
-              label: 'X',
-              href: 'https://twitter.com/nabla_hq',
+              label: "X",
+              href: "https://twitter.com/nabla_hq",
             },
             {
-              label: 'Linkedin',
-              href: 'https://www.linkedin.com/company/87381409',
+              label: "Linkedin",
+              href: "https://www.linkedin.com/company/87381409",
             },
             {
-              label: 'Instagram',
-              href: 'https://www.instagram.com/nabla_hq/',
+              label: "Instagram",
+              href: "https://www.instagram.com/nabla_hq/",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/nabla-studio/quirks',
+              label: "GitHub",
+              href: "https://github.com/nabla-studio/quirks",
             },
           ],
         },
@@ -121,16 +119,28 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
     metadata: [
-      {name: 'description', content: 'Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.'},
-      {name: 'og:title', content: 'Quirks'},
-      {name: 'og:site_name', content: 'Quirks'},
-      {name: 'og:description', content: 'Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.'},
-      {name: 'og:type', content: 'website'},
-      {name: 'twitter:title', content: 'Quirks'},
-      {name: 'twitter:site', content: '@nabla_hq'},
-      {name: 'twitter:description', content: 'Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.'},
-      {name: 'twitter:card', content: 'summary_large_image'},
-    ]
+      {
+        name: "description",
+        content:
+          "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+      },
+      { name: "og:title", content: "Quirks" },
+      { name: "og:site_name", content: "Quirks" },
+      {
+        name: "og:description",
+        content:
+          "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+      },
+      { name: "og:type", content: "website" },
+      { name: "twitter:title", content: "Quirks" },
+      { name: "twitter:site", content: "@nabla_hq" },
+      {
+        name: "twitter:description",
+        content:
+          "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+      },
+      { name: "twitter:card", content: "summary_large_image" },
+    ],
   } satisfies Preset.ThemeConfig,
 };
 
